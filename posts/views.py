3516c30b5a082d4af2f801a150b28ebeee8de0d9
Daniel009891 <danielotowski@googlemail.com>
2024-03-31 11:48:56 +0000

    Add permissions to posts so logged out users cannot post

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
@@ -7,6 +7,9 @@ from .serializers import PostSerializer
 
 class PostList(APIView):
     serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
     
     def get(self, request):
         posts = Post.objects.all()
