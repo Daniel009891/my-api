d81542b09997ffece566e40358cc6f4716a9267d
Daniel009891 <danielotowski@googlemail.com>
2024-04-01 19:31:16 +0000

    Add like detail view

--- likes/views.py
+++ likes/views.py
@@ -8,3 +8,6 @@ class LikeList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
