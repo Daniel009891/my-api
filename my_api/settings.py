a7df23607682275b3ea8cf317bbebf03d3ba2713
Daniel otowski <danielotowski@googlemail.com>
2024-04-06 21:08:40 +0000

    Test for deployment to heroku

--- my_api/settings.py
+++ my_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://pooch-book-api-b940e6d3707a.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'pooch-book-api-b940e6d3707a.herokuapp.com']
 
 
 # Application definition
