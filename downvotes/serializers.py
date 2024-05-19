a312c39ebfa7cfc09d5628a83e70cc0e35dfaae2
Daniel otowski <danielotowski@googlemail.com>
2024-05-19 15:55:54 +0000

    Add downvote serializer class

--- downvotes/serializers.py
+++ downvotes/serializers.py
@@ -2,3 +2,8 @@ from django.db import IntegrityError
 from rest_framework import serializers
 from downvotes.models import DownVote
 
+
+class DownVoteSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    
