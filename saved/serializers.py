69ff7ca84646e59275a35629920cc498e1b4c801
Daniel otowski <danielotowski@googlemail.com>
2024-05-20 10:44:55 +0000

    Add create function to saved serializer

--- saved/serializers.py
+++ saved/serializers.py
@@ -6,4 +6,15 @@ from saved.models import Saved
 class SavedSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
-    
+    class Meta:
+        model = Saved
+        fields = [
+            'id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
