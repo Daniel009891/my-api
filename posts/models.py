ca68ecb530a7accab41fb17e986bd148d6eacd38
Daniel otowski <danielotowski@googlemail.com>
2024-05-21 19:37:22 +0000

    Change default post image

--- posts/models.py
+++ posts/models.py
@@ -36,7 +36,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to="images/", default="../default_post_vqx2hv.jpg", blank=True
+        upload_to="images/", default="../default_post_vqx2hv", blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default="normal"
